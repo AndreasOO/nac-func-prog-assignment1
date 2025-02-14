254ceca6921f331f16ca7b98b58cae6b1972d9b1
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-02-14 21:50:25 +0100

    Added tests for empty list and positive test flow.
    Still need to do corner cases tests

--- test/MovieDataHandlerTest.java
+++ test/MovieDataHandlerTest.java
@@ -1,8 +1,12 @@
-import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 class MovieDataHandlerTest {
 
@@ -15,8 +19,8 @@ class MovieDataHandlerTest {
             new Movie("573a139ef29313caabcfc154", "Duplicate Movie Title", 1975, List.of("Romance", "Comedy"), "John Joe", List.of("Single Actor in most movies", "Its late"), 5.8, List.of("English", "Swedish"), 120),
             new Movie("573a139ef29313caabcfc155", "Movie Title With Longest Runtime", 1975, List.of("Romance", "Comedy"), "John Joe", List.of("Single Actor in most movies", "Bad Name"), 8.8, List.of("English", "Swedish"), 220),
             new Movie("573a139ef29313caabcfc156", "Movie Title With Highest IMDB rating", 1975, List.of("Romance", "Comedy"), "John Joe", List.of("Single Actor in most movies", "What"), 9.8, List.of("English", "Swedish"), 120),
-            new Movie("573a139ef29313caabcfc157", "Movie Title 1", 1975, List.of("Romance", "Comedy"), "John Joe", List.of("Single Actor in most movies", "Texas Ranger"), 1.8, List.of("English", "Swedish"), 120),
-            new Movie("573a139ef29313caabcfc166", "Movie Title 1", 1975, List.of("Romance", "Comedy"), "John Joe", List.of("Single Actor in most movies", "Milky Way"), 3.8, List.of("English", "Swedish"), 120)
+            new Movie("573a139ef29313caabcfc157", "Movie Title 3", 1975, List.of("Romance", "Comedy"), "John Joe", List.of("Single Actor in most movies", "Texas Ranger"), 1.8, List.of("English", "Swedish"), 120),
+            new Movie("573a139ef29313caabcfc166", "Movie Title 4", 1975, List.of("Romance", "Comedy"), "John Joe", List.of("Single Actor in most movies", "Milky Way"), 3.8, List.of("English", "Swedish"), 120)
     );
 
 
@@ -25,46 +29,65 @@ class MovieDataHandlerTest {
 
     @Test
     void findNumberOfMoviesByYear() {
-            // 8
+        assertEquals(8, movieDataHandler.findNumberOfMoviesByYear(movieListPositiveTestFlow, 1975));
+        assertEquals(0, movieDataHandler.findNumberOfMoviesByYear(emptyMovieList, 1975));
+
     }
 
     @Test
     void findRuntimeOfLongestMovie() {
-        // 220
+        assertEquals(220, movieDataHandler.findRuntimeOfLongestMovie(movieListPositiveTestFlow));
+        assertEquals(0, movieDataHandler.findRuntimeOfLongestMovie(emptyMovieList));
+
     }
 
     @Test
     void findNumberOfUniqueGenresByYear() {
-        // 2
+        assertEquals(2, movieDataHandler.findNumberOfUniqueGenresByYear(movieListPositiveTestFlow, 1975));
+        assertEquals(0, movieDataHandler.findNumberOfUniqueGenresByYear(emptyMovieList, 1975));
+
     }
 
     @Test
     void findActorsByHighestRatedMovie() {
-        // "Single Actor in most movies", "What"
+        assertEquals(List.of("Single Actor in most movies", "What"), movieDataHandler.findActorsByHighestRatedMovie(movieListPositiveTestFlow));
+        assertEquals(List.of(), movieDataHandler.findActorsByHighestRatedMovie(emptyMovieList));
+
     }
 
     @Test
     void findMovieWithLeastNumberOfActors() {
-        // "Movie Title With Least Actors"
+        assertEquals("Movie Title With Least Actors", movieDataHandler.findMovieWithLeastNumberOfActors(movieListPositiveTestFlow));
+
+        //replace with NoSuchElementException that comes with Optional.elseThrow()?
+        assertNull(movieDataHandler.findMovieWithLeastNumberOfActors(emptyMovieList));
     }
 
     @Test
     void findNumberOfActorsStarringInTwoOrMoreMovies() {
-        // 2
+        assertEquals(2, movieDataHandler.findNumberOfActorsStarringInTwoOrMoreMovies(movieListPositiveTestFlow));
+        assertEquals(0, movieDataHandler.findNumberOfActorsStarringInTwoOrMoreMovies(emptyMovieList));
     }
 
     @Test
     void findActorsFoundInMostMovies() {
-        // Single Actor in most movies"
+        assertEquals("Single Actor in most movies", movieDataHandler.findActorsFoundInMostMovies(movieListPositiveTestFlow));
+
+        //replace with NoSuchElementException that comes with Optional.elseThrow()?
+        assertNull(movieDataHandler.findActorsFoundInMostMovies(emptyMovieList));
+
+        // add duplicate with the joining...
     }
 
     @Test
     void findNumberOfUniqueLanguagesInMovies() {
-        // 2
+        assertEquals(2, movieDataHandler.findNumberOfUniqueLanguagesInMovies(movieListPositiveTestFlow));
+        assertEquals(0, movieDataHandler.findNumberOfUniqueLanguagesInMovies(emptyMovieList));
     }
 
     @Test
     void moviesHaveDuplicatesOfTitles() {
-        // true
+        assertTrue(movieDataHandler.moviesHaveDuplicatesOfTitles(movieListPositiveTestFlow));
+        assertFalse(movieDataHandler.moviesHaveDuplicatesOfTitles(emptyMovieList));
     }
 }
