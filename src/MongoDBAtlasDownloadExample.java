53bb3c00f0ac266394e97f2c3d51cdeeed161230
Andreas Ohlander <andreas.ohlander@gmail.com>
2025-02-14 15:33:53 +0100

    Initial commit with working db connection

---
+++ src/MongoDBAtlasDownloadExample.java
@@ -0,0 +1,43 @@
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MongoDBAtlasDownloadExample {
+    public MongoDBAtlasDownloadExample() {
+
+        //Skriv in rätt url!
+        String uri = "mongodb+srv://andreasohlander:test1234@cluster0.tmn3y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> moviesCollection = database.getCollection("movies");
+
+            //Get all movies from 1975
+            List<Movie> movieList = new ArrayList<>();
+            for (Document doc : moviesCollection.find(new Document("year", 1975))) {
+                {
+                    movieList.add(Movie.fromDocument(doc));
+                }
+            }
+
+            // Skriver ut alla filmer
+            for (Movie movie : movieList) {
+                System.out.println(movie);
+            }
+
+            //Här gör du anrop till alla dina funktioner som ska skriva ut svaren på frågorna som
+            //efterfrågas i uppgiften
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MongoDBAtlasDownloadExample m = new MongoDBAtlasDownloadExample();
+    }
+}
